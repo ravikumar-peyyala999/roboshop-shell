f18c44cd999c306aa738760e50a3266c61c1be9f
Ravi Kumar Peyyala <ravikumar.peyyala@outlook.com>
2023-09-03 10:05:04 +0530

    updated R53 records creation

--- create-ec2.sh
+++ create-ec2.sh
@@ -25,7 +25,7 @@ do
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Name": "'$i.$DOMAIN_NAME.'",
                             "Type": "A",
                             "TTL": 60,
                             "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
